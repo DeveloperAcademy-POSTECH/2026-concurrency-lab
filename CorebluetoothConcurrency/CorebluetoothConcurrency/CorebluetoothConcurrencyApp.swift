a2971188f1ffded60384d6199a0424d04a31f5f3
sun <sts07190@naver.com>
2026-06-18 21:31:19 +0900

    ADD: initial commit

---
+++ CorebluetoothConcurrency/CorebluetoothConcurrency/CorebluetoothConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CorebluetoothConcurrencyApp.swift
+//  CorebluetoothConcurrency
+//
+//  Created by sun on 6/18/26.
+//
+
+import SwiftUI
+
+@main
+struct CorebluetoothConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
